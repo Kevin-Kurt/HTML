7108b1bcd954c726d908d5daaa15dbe68b3cf98d
Kevin-Kurt <you@example.com>
2021-06-18 17:09:51 -0300

    confirmar

---
+++ script.js
@@ -0,0 +1,12 @@
+let foto = document.querySelector('#foto')
+
+function fotoZoom() {
+    foto.style.width = '400px'
+    foto.style.height = '300px'
+}
+
+function fotoNormal() {
+    foto.style.width = '300px'
+    foto.style.height = '200px'
+}
+
