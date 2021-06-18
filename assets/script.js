ae66cdb87186e5ba42bb2090810c83216dc685fa
Kevin-Kurt <you@example.com>
2021-06-18 19:40:39 -0300

    confirmar

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,3 @@
-document.getElementById('bnt').onclick = teste
-
-function validaEmail() {
-    let txtEmail = document.querySelector('#txtEmail')
-    if (txtEmail.value.indexOf('@') == -1){
-        txtEmail.innerHTML = "E-mail invalido"
-        txtEmail.style.color = 'red"'
-    }
-
-}
+document.getElementById('texto').style.color='#f00';
 
 
