18e370493431b9ca799b9c8ea089f88163aae1ca
Kevin-Kurt <you@example.com>
2021-06-21 19:50:52 -0300

    em progresso

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
+//let textocentro = document.querySelector('#textocentro');
 
-document.getElementById('texto').style.color='#FFFFFF';
+let textoKennen = document.querySelector(".textoKennen")
+let textoEvelynn = document.querySelector(".textoEvelynn")
+let textoIrelia = document.querySelector(".textoIrelia")
 
 
+
+textoIrelia.style.textAlign = "justify"
+textoKennen.style.textAlign = "justify"
+textoEvelynn.style.textAlign = "justify"
+
+
+//texto.style.color = '#FFFFFF'
