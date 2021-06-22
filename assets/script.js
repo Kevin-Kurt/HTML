7931cc96a94bf5c90d22b092df6e90201e93ad8f
Kevin-Kurt <you@example.com>
2021-06-22 19:44:06 -0300

    treino

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,22 @@
 let textoKennen = document.querySelector(".textoKennen")
 let textoEvelynn = document.querySelector(".textoEvelynn")
 let textoIrelia = document.querySelector(".textoIrelia")
+let video = document.querySelector("#video")
+let container = document.querySelector(".container")
+let barra1 = document.querySelector(".barra1")
 
 
+barra1.style.margin = '0px 0px 0px 0px'
+barra1.style.padding = '10px 0px 10px 0px'
+barra1.style.textAlign = "center"
+video.style.textAlign = "center"
+video.style.position = "absolute"
 
-textoIrelia.style.textAlign = "justify"
-textoKennen.style.textAlign = "justify"
-textoEvelynn.style.textAlign = "justify"
+//container.style.position = "absolute"
+//container.style.margin = "auto"
+//textoIrelia.style.textAlign = "justify"
+//textoKennen.style.textAlign = "justify"
+//textoEvelynn.style.textAlign = "justify"
 
 
 //texto.style.color = '#FFFFFF'
